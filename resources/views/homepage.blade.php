5f334094779b3c36fb7db809482f6312b33ff28a
Seme87 <massimomonteleone@hotmail.com>
2023-01-14 02:02:15 +0100

    Aggiunge Header e jumbotron con button position absolute

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,13 @@
-@extends('layouts.main');
+@extends('layouts.main')
 
 @section('page-title')
     Home Page
+@endsection
+
+@section('page-content')
+    <section class="section-main">
+        <div class="container">
+            <button class=" ms-btn-series">CURRENT SERIES</button>
+        </div>
+    </section>
 @endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,57 @@
+<header>
+    <section class="ms-top-header">
+        {{-- sezione top header --}}
+        <div class="container">
+            <nav class="ms-nav-header-top">
+                <li><a href="#">DC POWER VISA®</a></li>
+
+                <li class="dropdown p-e-0">
+                    <a class="dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        ADDICTIONAL DC SITES
+                    </a>
+                    <ul class="dropdown-menu p-e-0">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+            </nav>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <nav class="ms-nav-header-bottom">
+                <ul>
+                    <li><a href="#"><img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt=""></a></li>
+                    <li><a href="">CHARACTERS</a></li>
+                    <li><a href="">COMICS</a></li>
+                    <li><a href="">MOVIES</a></li>
+                    <li><a href="">TV</a></li>
+                    <li><a href="">GAMES</a></li>
+                    <li><a href="">COLLECTIBLES</a></li>
+                    <li><a href="">VIDEOS</a></li>
+                    <li><a href="">FANS</a></li>
+                    <li><a href="">NEWS</a></li>
+                    <li class="dropdown p-e-0">
+                        <a class="dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            ADDICTIONAL DC SITES
+                        </a>
+                        <ul class="dropdown-menu p-e-0">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <form action="">
+                            <input class="search-text " type="text" placeholder="Search">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+
+                        </form>
+                    </li>
+                   
+                </ul>
+            </nav>
+        </div>
+    </section>
+</header>
